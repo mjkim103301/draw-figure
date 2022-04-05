3de901e80ae777075f4e4cff70541073eff43740
김민지(2210188) <mjkim103301@hanwha.com>
2022-04-05 16:22:48 +0900

    chore: init project

---
+++ src/main/java/com/study/drawfigure/DrawFigureApplication.java
@@ -0,0 +1,13 @@
+package com.study.drawfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawFigureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrawFigureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/drawfigure/DrawFigureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.drawfigure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawFigureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
